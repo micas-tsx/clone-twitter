703d18ab56db58e650e0abb26e539823224562bb
micas-tsx <micaelabud21@gmail.com>
2024-12-22 20:57:55 -0300

    layout-left-side

--- yt-clone-twitter-frontend/src/app/(ui)/layout.tsx
+++ yt-clone-twitter-frontend/src/app/(ui)/layout.tsx
@@ -1,4 +1,7 @@
 import { ReactNode } from "react"
+import { Logo } from "@/components/ui/logo"
+import { NavItem } from "@/components/nav/nav-item"
+import { faHouse, faUser } from "@fortawesome/free-solid-svg-icons"
 
 type Props = {
     children: ReactNode
@@ -7,7 +10,26 @@ type Props = {
 export default function Layout({ children }: Props) {
     return (
         <main className="min-h-screen flex justify-center max-auto mx-w-7xl">
-            <section className="hidden lg:flex flex-col sticky top-0 h-screen w-72 px-3 border-r-2 border-gray-900">...</section>
+            <section className="hidden lg:flex flex-col sticky top-0 h-screen w-72 px-3 border-r-2 border-gray-900">
+                <div className="flex-1 mt-6">
+                    <Logo size={24} />
+                    <nav className="mt-11">
+                        <NavItem 
+                            href="/home"
+                            icon={faHouse}
+                            label="Página inicial"
+                        />
+                        <NavItem 
+                            href="/profile"
+                            icon={faUser}
+                            label="Meu perfil"
+                        />
+                    </nav>
+                </div>
+                <div>
+                    ...
+                </div>
+            </section>
             <section className="flex-1 max-w-lg">{children}</section>
             <aside className="hidden lg:flex flex-col gap-6 sticky top-0 h-fit w-96 px-8 py-6 border-l-2 border-gray-900">...</aside>
         </main>
