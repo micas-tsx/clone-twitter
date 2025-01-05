2b42b8866edb1fc389f2a20a98cac99f7b9d1cbd
micas-tsx <micaelabud21@gmail.com>
2025-01-05 17:08:01 -0300

    tweet-post-2

--- yt-clone-twitter-frontend/src/components/tweet/tweet-item.tsx
+++ yt-clone-twitter-frontend/src/components/tweet/tweet-item.tsx
@@ -1,12 +1,24 @@
+'use client'
+
 // biome-ignore lint/style/useImportType: <explanation>
 import { Tweet } from "@/types/tweet"
+import { faComment, faHeart } from "@fortawesome/free-regular-svg-icons"
+import { faRetweet, faHeart as faHeartFilled } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import Link from "next/link"
+import { useState } from "react"
 
 type Props = {
     tweet: Tweet
 }
 
 export const TweetItem = ({ tweet }: Props) => {
+    const [liked, setLiked] = useState(tweet.liked)
+
+    const handleLikeButton = () => {
+        setLiked(!liked)
+    }
+    
     return(
         <div className="flex gap-2 p-6 border-b-2 border-gray-900">
             <div>
@@ -36,7 +48,27 @@ export const TweetItem = ({ tweet }: Props) => {
                     </div>
                 }
                 <div className="flex mt-6 text-gray-500">
-                    ...
+                    <div className="flex-1">
+                        <Link href={`/tweet/${tweet.id}`}>
+                            <div className="inline-flex items-center gap-2 cursor-pointer">
+                                <FontAwesomeIcon icon={faComment} className="size-6" />
+                                <div className="text-lg">{tweet.commentCount}</div>
+                            </div>
+                        </Link>
+                    </div>
+                    <div className="flex-1">
+                            <div className="inline-flex items-center gap-2 cursor-pointer">
+                                <FontAwesomeIcon icon={faRetweet} className="size-6" />
+                                <div className="text-lg">{tweet.retweetCount}</div>
+                            </div>
+                    </div>
+                    <div className="flex-1">
+                        {/* biome-ignore lint/a11y/useKeyWithClickEvents: <explanation> */}
+                            <div onClick={handleLikeButton} className={`inline-flex items-center gap-2 cursor-pointer ${liked && 'text-red-400'}`}>
+                            <FontAwesomeIcon icon={liked ? faHeartFilled : faHeart} className="size-6" />
+                            <div className="text-lg">{tweet.likeCount}</div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
