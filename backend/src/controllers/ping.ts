086940bb5a62372154204d341487256ad0009eae
micas-tsx <micaelabud21@gmail.com>
2025-07-17 08:40:18 -0300

    refac: change frontend to root directoy

--- backend/src/controllers/ping.ts
+++
@@ -1,6 +0,0 @@
-// biome-ignore lint/style/useImportType: <explanation>
-import { Request, Response } from "express";
-
-export const ping = (req:Request, res:Response) => {
-    res.json({ pong: true })
-}
--- backend/src/router/main.ts
+++
@@ -1,6 +0,0 @@
-import { Router } from 'express'
-import * as pingController from '../controllers/ping'
-
-export const mainRouter = Router()
-
-mainRouter.get('/ping', pingController.ping)
